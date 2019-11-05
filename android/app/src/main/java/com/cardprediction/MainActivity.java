50954995dcf71aa567ef05c652c4fe4a855be668
Tyler Kuhn <tyler@wargoats.com>
2019-11-04 21:34:50 -0800

    go fullscreen

--- android/app/src/main/java/com/cardprediction/MainActivity.java
+++ android/app/src/main/java/com/cardprediction/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cardprediction;
 
+import android.os.Bundle;
+import android.view.View;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -24,5 +26,26 @@ public class MainActivity extends ReactActivity {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
         };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideNavigationBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+
+    private void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 }
